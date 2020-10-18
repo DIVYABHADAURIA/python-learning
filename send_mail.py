16401e2b56682ff181e6adcad44c3b3a04d42958
DIVYABHADAURIA <65533983+DIVYABHADAURIA@users.noreply.github.com>
2020-10-18 19:17:08 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -3,19 +3,24 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart	
 username='divya.pythonlearning@gmail.com'
 password='Admin11!'
-msg_str="Hi ..this is dummy mail .for python learning purpose"
+msg_str="Hi ..This is dummy mail .for python learning purpose"
 
-def send_mail(text='Email Body',subject='Hello',from_email='Divya Bhadauria <divya.pythonlearning@gmail.com>',to_emails=None):
-	 assert isintance(to_emails,list)
-     #login to my SMTP server
-      msg = MIMEMultipart('alternative')
-      msg['From'] = from_email
-      msg['To'] =", ".join(to_emails)
-      server = smtplib.SMTP("smtp.gmail.com", 587, timeout=120)
-      server.ehlo()
-      server.starttls()
-      server.login(username,password)
-      server.sendmail(from_email,to_emails,msg_str)
+def send_mail(text='Email Body',subject='Hello World',from_email='Divya Bhadauria <divya.pythonlearning@gmail.com>',to_emails=None):
+	 assert isinstance(to_emails,list)
+	 msg = MIMEMultipart("alternative")
+	 msg['From'] = from_email
+	 msg['To'] =", ".join(to_emails)
+	 msg['Subject']=subject
+	 text_part= MIMEText(text,'plain')
+	 msg.attach(text_part)
+	 html_part= MIMEText("<h1> This is heading </h1>",'html')
+	 msg.attach(html_part)
+	 msg_str=msg.as_string()
+	 server = smtplib.SMTP("smtp.gmail.com", 587, timeout=120)
+	 server.ehlo()
+	 server.starttls()
+	 server.login(username,password)
+	 server.sendmail(from_email,to_emails,msg_str)
 
 
-      server.quit()
+	 server.quit()
